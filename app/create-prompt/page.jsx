647fd35be782b9b89a35bf72d1de610b7daa23b3
hashaam <hashaamkhan4247@gmail.com>
2023-07-27 14:27:47 +0500

    created api for posting/creating a prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -13,7 +13,32 @@ const CreatePrompt = () => {
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userID: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
---
+++ models/prompt.js
@@ -0,0 +1,19 @@
+const { Schema, default: mongoose, models, model } = require("mongoose");
+
+const PromptSchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required"],
+  },
+});
+
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
+export default Prompt;
